c1d54dd05b37b5bc9243f7be83273b877a506136
abarragree <anthony.barragree@charter.com>
2018-03-16 10:33:22 -0600

    full path

--- ssh-key-copy.py
+++ ssh-key-copy.py
@@ -21,7 +21,7 @@ def read_key(pub_keys, new_keys):
 
 def get_keys():
     # Go to each host and generate a key
-    ssh_key_path = '.ssh/id_rsa'
+    ssh_key_path = '/home/ec2-user/.ssh/id_rsa'
     localpath = './remote_keys/'
     run("ssh-keygen -f {} -t rsa -N ''".format(ssh_key_path))
     # Copy pub keys here.
